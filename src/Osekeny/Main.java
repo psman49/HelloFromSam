80b3f9d8b7e6ed25ab80abfa60ecaf28bfc435a9
Sam Osekeny <samuelpo49@gmail.com>
2021-10-16 11:37:58 -0600

    Initial commit

---
+++ src/Osekeny/Main.java
@@ -0,0 +1,9 @@
+package Osekeny;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello my name is Samuel Osekeny");
+    }
+}
